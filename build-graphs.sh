623ef79e0ff392376c18f9b4da13d11db99236fd
Paul Newton <paul@topolino.org>
2020-04-10 22:59:55 +0200

    Added horizontal rule

--- build-graphs.sh
+++ build-graphs.sh
@@ -9,6 +9,10 @@ git pull
 # build graphs
 jupyter nbconvert --to html covid.ipynb
 
+# add timestamp to HTML file and store in docs folder
+TS=`date`
+sed "s_<h1 id=\"COVID-19-ML\"_Last updated\: $TS<hr>&_" covid.html > docs/covid.html
+
 # commit to git
 git add docs/* covid.html
 git commit -m "Updated graphs"

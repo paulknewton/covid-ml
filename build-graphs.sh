b9ad05d6c22c0c68d7a5ce6631410325b3e351a0
Paul Newton <paul@topolino.org>
2020-04-10 23:38:03 +0200

    Updated timestamp logic. New worldwide graph

--- build-graphs.sh
+++ build-graphs.sh
@@ -2,7 +2,10 @@
 # Generate graphs and upload to github.com
 #
 #
-
+add_timestamp() { # input_file timestamp
+    TS=`date`
+    sed "s_<h1 id=\"COVID-19-ML\"_Last updated\: $TS<hr>&_" "$1"
+}
 # refresh code
 git pull
 
@@ -10,9 +13,8 @@ git pull
 jupyter nbconvert --to html covid.ipynb
 
 # add timestamp to HTML file and store in docs folder
-TS=`date`
-sed "s_<h1 id=\"COVID-19-ML\"_Last updated\: $TS<hr>&_" covid.html > docs/covid.html
-sed "s_TIMESTAMP_$TS _" docs/index_template.html > docs/index.html
+add_timestamp covid.html > docs/covid.html
+add_timestamp docs/index_template.html > docs/index.html
 
 # commit to git
 git add docs/* covid.html

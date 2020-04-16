d9535ff5660d56055c3d04a07748f3d93d772325
Paul Newton <paul@topolino.org>
2020-04-16 23:49:13 +0200

    Updated graphs

--- docs/_bin/build-graph-list.sh
+++ docs/_bin/build-graph-list.sh
@@ -4,12 +4,13 @@
 #
 
 PYTHON_SCRIPT=_bin/collections.py
+PYTHON_EXE=python3
 
 function build_yaml { # directory format output.yaml
   for i in "$1"
   do
     echo "Processing `basename $i`..."
-    find $i -name *.png | sort | python $PYTHON_SCRIPT --format $2 > _data/`basename "$i"`.yml
+    find $i -name *.png | sort | $PYTHON_EXE $PYTHON_SCRIPT --format $2 > _data/`basename "$i"`.yml
   done
 }
 

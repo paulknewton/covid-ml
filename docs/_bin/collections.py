f66035418edcbb2a7026ec1e8973bdb807668d64
Paul Newton <paul@topolino.org>
2020-04-29 20:11:41 +0200

    Removed markdown files from countries (too slow to build Jekyll)

--- docs/_bin/collections.py
+++ docs/_bin/collections.py
@@ -18,16 +18,19 @@ if __name__ == "__main__":
         # url: use the full path
         print("  url: %s" % file)
 
-        # notes: use the filename (no path, no suffix)
-        #notes_file = file.with_suffix(".md")
-        #print("  notes: %s" % notes_file)
-
-        # create a 'notes' file (if missing)
-        #Path(notes_file).touch(exist_ok=True)
-
         # if list of countries then add a country name
         if args.format == "countries":
             print("  anchor: %s" % filename_no_suffix[:filename_no_suffix.find("-") - 1:].replace("_", " "))
+
+        # notes are not used for countries
+        else:
+            # notes: use the filename (no path, no suffix)
+            notes_file = file.with_suffix(".md")
+            print("  notes: %s" % notes_file)
+
+            # create a 'notes' file (if missing)
+            Path(notes_file).touch(exist_ok=True)
+
 
 
 
